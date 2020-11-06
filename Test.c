fa614098c763621b74911310a20bf229ea6167b6
seanoushana <74024090+seanoushana@users.noreply.github.com>
2020-11-06 11:34:16 -0500

    Update Test.c

--- Test.c
+++ Test.c
@@ -2,5 +2,9 @@
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
+   int a = 5;
+   int b = 6;
+   int sum = a + b;
+   printf("%d", sum);
    return 0;
 }
